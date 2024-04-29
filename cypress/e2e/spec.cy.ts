223def5822b97e16c710cb9b57300710ae2b3a7f
Asrinne Nazari <asse_89@hotmail.com>
2024-04-29 09:46:31 +0200

    switched api key

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
-describe('template spec', () => {
+describe('input test', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:5173')
   })
 })
